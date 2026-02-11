ed4c25a6ebbc202e30b45e51427ce6911b2e6ba5
Sanoivan250 <157384434+Sanoivan250@users.noreply.github.com>
2026-02-11 11:04:27 +0200

    Create Attendance schema file

---
+++ backend/models/Attendance.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const attendanceSchema = new mongoose.Schema({
+    studentId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Student',
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['Present', 'Absent'],
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Attendance = mongoose.model('Attendance', attendanceSchema);
+
+module.exports = Attendance;
